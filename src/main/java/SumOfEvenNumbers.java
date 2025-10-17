d4ba4e20c43d14f36931b509b43686cca2752923
Khadija-1111 <sfkhadija.edu@gmail.com>
2025-10-17 19:47:35 -0400

    Update SumOfEvenNumbers.java

--- src/main/java/SumOfEvenNumbers.java
+++ src/main/java/SumOfEvenNumbers.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 
 public class SumOfEvenNumbers {
-    public static void main(String[] args) {     
+    public static void main(String[] args) {
         // TODO: Step 1 - Get user input
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer value: ");
+        int x = sc.nextInt();
+        int y = 0;
         // TODO: Step 2 - Validate input
-        
-        // TODO: Step 3 - Calculate sum of even numbers
-        
-        // TODO: Step 4 - Display the result
+        if (x < 2) {
+            System.out.println("Error: Input must be 2 or greater.");
+        } else {
+            for (int i = 2; i <= x; i += 2) {
+                y = y + i;
+            }
+            System.out.println("The sum of even numbers between 2 and " + x + " is " + y + ".");
+            // TODO: Step 3 - Calculate sum of even numbers
 
+            // TODO: Step 4 - Display the result
+
+        }
     }
 }
